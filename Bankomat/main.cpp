2875ceb90f7be32b5c71fbe4b64db54ccf1379ec
Pedr0o0o <lucasmarek@gmapil.com>
2020-12-18 18:00:16 +0100

    18,12,2020 takie byle co

--- Bankomat/main.cpp
+++ Bankomat/main.cpp
@@ -1,5 +1,116 @@
+#include<iostream>
+#include"osoba.cpp"
+
+using namespace std;
+
+int main() 
+{
+	int wybor;
+
+	cout << "wybierz rodzaj transakcji:"<<endl;
+	cout << "wybierz 1 - wplata" << endl;
+	cout << "wybierz 2 - wyplata" << endl;
+	cout << "wybierz 3 - sprawdzenie stanu konta" << endl;
+	cout << "wybierz 4 - anuluj" << endl;
 
 
-int main() {
+	cin >> wybor;
 
+	if (wybor > 0 && wybor < 5)
+	{
+
+		switch (wybor)
+		{
+		case 1://wplata
+		{
+
+			cout << "wybierz typ transakcji:" << endl;
+			cout << "wybierz 1 - karta" << endl;
+			cout << "wybierz 2 - blik" << endl;
+			cout << "wybierz 3 - anuluj" << endl;
+
+			cin >> wybor;
+
+			if (wybor > 0 && wybor < 4)
+			{
+				switch (wybor)
+				{
+				case 1://karta
+				{
+					break;
+				}
+				case 2://blik
+				{
+					break;
+				}
+
+				case 3://anuluj
+				{
+					break;
+				}
+
+				}
+			}
+			else cout << "nieprawidlowy wybor" << endl;
+
+			break;
+		}
+
+		case 2://wyplata
+		{
+
+			cout << "wybierz typ transakcji:" << endl;
+			cout << "wybierz 1 - karta" << endl;
+			cout << "wybierz 2 - blik" << endl;
+			cout << "wybierz 3 - anuluj" << endl;
+
+			cin >> wybor;
+
+			if (wybor > 0 && wybor < 4)
+			{
+				switch (wybor)
+				{
+				case 1://karta
+				{
+					break;
+				}
+
+				case 2://blik
+				{
+
+					break;
+				}
+
+				case 3://anuluj
+				{
+					break;
+				}
+
+				}
+			}
+			else cout << "nieprawidlowy wybor" << endl;
+	
+			break;
+		}
+
+		case 3://wyœwietl stan konta
+		{
+			//insert karta
+			//werify karta
+			// osoba.wyswietl_stan_konta();
+
+			break;
+		}
+
+		case 4://anuluj
+		{
+			break;
+		}
+		}
+
+	}
+	else cout << "nieprawidlowy wybor" << endl;
+
+
+	return 0;
 }
---
+++ Bankomat/osoba.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class klient
+{
+public:
+
+	void wyswietl_stan_konta(double stan_konta);
+	bool weryfikacja(int karta,int insert_pin);
+
+private:
+
+	string imie;
+	string nazwisko;
+
+	long int nr_konta;
+	double stan_konta;
+
+	int nr_karty;
+	int pin;
+
+};
+
+void wyswietl_stan_konta(double stan_konta)
+{
+	cout << " stan konta wynosi:" << endl;
+	cout << stan_konta;
+}
+
+bool weryfikacja(int karta,int insert_pin)
+{
+	if (nr_karty == karta)
+	{
+		if (pin == insert_pin)
+		{
+			return true;
+		}
+		else return false;
+	}
+	else return false;
+
+}
