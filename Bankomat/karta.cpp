ac1cb62767578a85db220df62d7ff4d7b27c9b97
Pedr0o0o <lucasmarek@gmapil.com>
2020-12-20 14:53:05 +0100

    k

---
+++ Bankomat/karta.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class karta
+{
+public:
+
+private:
+
+	int nr_karty_bankomatowej;
+	int pin;
+
+};
+
--- Bankomat/main.cpp
+++ Bankomat/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include"osoba.cpp"
+#include"karta.cpp"
+
 
 using namespace std;
 
@@ -7,110 +9,117 @@ int main()
 {
 	int wybor;
 
-	cout << "wybierz rodzaj transakcji:"<<endl;
-	cout << "wybierz 1 - wplata" << endl;
-	cout << "wybierz 2 - wyplata" << endl;
-	cout << "wybierz 3 - sprawdzenie stanu konta" << endl;
-	cout << "wybierz 4 - anuluj" << endl;
-
-
-	cin >> wybor;
-
-	if (wybor > 0 && wybor < 5)
+	do 
 	{
 
-		switch (wybor)
+		cout << "wybierz rodzaj transakcji:" << endl;
+		cout << "wybierz 1 - wplata" << endl;
+		cout << "wybierz 2 - wyplata" << endl;
+		cout << "wybierz 3 - sprawdzenie stanu konta" << endl;
+		cout << "wybierz 4 - anuluj" << endl;
+
+
+		cin >> wybor;
+
+		if (wybor > 0 && wybor < 5)
 		{
-		case 1://wplata
-		{
 
-			cout << "wybierz typ transakcji:" << endl;
-			cout << "wybierz 1 - karta" << endl;
-			cout << "wybierz 2 - blik" << endl;
-			cout << "wybierz 3 - anuluj" << endl;
-
-			cin >> wybor;
-
-			if (wybor > 0 && wybor < 4)
+			switch (wybor)
 			{
-				switch (wybor)
-				{
-				case 1://karta
-				{
-					break;
-				}
-				case 2://blik
-				{
-					break;
-				}
-
-				case 3://anuluj
-				{
-					break;
-				}
-
-				}
-			}
-			else cout << "nieprawidlowy wybor" << endl;
-
-			break;
-		}
-
-		case 2://wyplata
-		{
-
-			cout << "wybierz typ transakcji:" << endl;
-			cout << "wybierz 1 - karta" << endl;
-			cout << "wybierz 2 - blik" << endl;
-			cout << "wybierz 3 - anuluj" << endl;
-
-			cin >> wybor;
-
-			if (wybor > 0 && wybor < 4)
+			case 1://wplata
 			{
-				switch (wybor)
-				{
-				case 1://karta
-				{
-					break;
-				}
 
-				case 2://blik
+				cout << "wybierz typ transakcji:" << endl;
+				cout << "wybierz 1 - karta" << endl;
+				cout << "wybierz 2 - blik" << endl;
+				cout << "wybierz 3 - anuluj" << endl;
+
+				cin >> wybor;
+
+				if (wybor > 0 && wybor < 4)
 				{
+					switch (wybor)
+					{
+					case 1://karta
+					{
+						break;
+					}
+					case 2://blik
+					{
+						break;
+					}
 
-					break;
-				}
+					case 3://anuluj
+					{
+						break;
+					}
 
-				case 3://anuluj
-				{
-					break;
+					}
 				}
+				else cout << "nieprawidlowy wybor" << endl;
 
-				}
+				break;
 			}
-			else cout << "nieprawidlowy wybor" << endl;
-	
-			break;
+
+			case 2://wyplata
+			{
+
+				cout << "wybierz typ transakcji:" << endl;
+				cout << "wybierz 1 - karta" << endl;
+				cout << "wybierz 2 - blik" << endl;
+				cout << "wybierz 3 - anuluj" << endl;
+
+				cin >> wybor;
+
+				if (wybor > 0 && wybor < 4)
+				{
+					switch (wybor)
+					{
+					case 1://karta
+					{
+						break;
+					}
+
+					case 2://blik
+					{
+
+						break;
+					}
+
+					case 3://anuluj
+					{
+						break;
+					}
+
+					}
+				}
+				else cout << "nieprawidlowy wybor" << endl;
+
+				break;
+			}
+
+			case 3://wyœwietl stan konta
+			{
+				//insert karta
+				//werify karta
+				// osoba.wyswietl_stan_konta();
+
+				break;
+			}
+
+			case 4://anuluj
+			{
+				break;
+			}
+			}
+
 		}
+		else cout << "nieprawidlowy wybor" << endl;
 
-		case 3://wyœwietl stan konta
-		{
-			//insert karta
-			//werify karta
-			// osoba.wyswietl_stan_konta();
 
-			break;
-		}
+		system("pause");
 
-		case 4://anuluj
-		{
-			break;
-		}
-		}
-
-	}
-	else cout << "nieprawidlowy wybor" << endl;
-
+	}while (true);
 
 	return 0;
 }
